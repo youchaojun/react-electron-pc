8ef1d0778d17c807452a7000ea498e3d00d57c19
youchaojun <597880581@qq.com>
2021-02-10 11:25:12 +0800

    增加说明文档

--- global.d.ts
+++ global.d.ts
@@ -22,11 +22,4 @@ declare global {
   }
 }
 
-// 这个必须有，将文件转化为模块
 export {};
-
-// ts-server会根据tsconfig的include去包含 d.ts/ts文件
-// d.ts 文件分全局的和模块的
-// 没有用到module语法的默认都是全局的
-// 全局的你可以通过合并接口给window加属性 或者声明新的全局变量....
-// 如果想在module级别的声明文件里修改全局的 才需要declare global ...
